3f8212f27b62d657cca5023745530e383664f3d5
David De Anda <omarandstuff@gmail.com>
2023-09-27 00:15:02 -0600

    Test refines, decribe actual symbols

--- tests/generate.test.ts
+++ tests/generate.test.ts
@@ -1,38 +1,36 @@
 import { generate } from '../src'
 
-describe('time-based-one-time-password', (): void => {
-  describe('generate', (): void => {
-    it('generates the right codes for the given times', async (): Promise<void> => {
-      const secret = Buffer.from('3132333435363738393031323334353637383930', 'hex').toString('utf-8')
-      const secret32 = Buffer.from('3132333435363738393031323334353637383930313233343536373839303132', 'hex').toString('utf-8')
-      const secret64 = Buffer.from(
-        '31323334353637383930313233343536373839303132333435363738393031323334353637383930313233343536373839303132333435363738393031323334',
-        'hex'
-      ).toString('utf-8')
+describe(generate, (): void => {
+  it('generates the right codes for the given times', async (): Promise<void> => {
+    const secret = Buffer.from('3132333435363738393031323334353637383930', 'hex').toString('utf-8')
+    const secret32 = Buffer.from('3132333435363738393031323334353637383930313233343536373839303132', 'hex').toString('utf-8')
+    const secret64 = Buffer.from(
+      '31323334353637383930313233343536373839303132333435363738393031323334353637383930313233343536373839303132333435363738393031323334',
+      'hex'
+    ).toString('utf-8')
 
-      expect(generate(secret, { algorithm: 'sha1', codeDigits: 8, time: 59 })).toEqual('94287082')
-      expect(generate(secret32, { algorithm: 'sha256', codeDigits: 8, time: 59 })).toEqual('46119246')
-      expect(generate(secret64, { algorithm: 'sha512', codeDigits: 8, time: 59 })).toEqual('90693936')
+    expect(generate(secret, { algorithm: 'sha1', codeDigits: 8, time: 59 })).toEqual('94287082')
+    expect(generate(secret32, { algorithm: 'sha256', codeDigits: 8, time: 59 })).toEqual('46119246')
+    expect(generate(secret64, { algorithm: 'sha512', codeDigits: 8, time: 59 })).toEqual('90693936')
 
-      expect(generate(secret, { algorithm: 'sha1', codeDigits: 8, time: 1111111109 })).toEqual('07081804')
-      expect(generate(secret32, { algorithm: 'sha256', codeDigits: 8, time: 1111111109 })).toEqual('68084774')
-      expect(generate(secret64, { algorithm: 'sha512', codeDigits: 8, time: 1111111109 })).toEqual('25091201')
+    expect(generate(secret, { algorithm: 'sha1', codeDigits: 8, time: 1111111109 })).toEqual('07081804')
+    expect(generate(secret32, { algorithm: 'sha256', codeDigits: 8, time: 1111111109 })).toEqual('68084774')
+    expect(generate(secret64, { algorithm: 'sha512', codeDigits: 8, time: 1111111109 })).toEqual('25091201')
 
-      expect(generate(secret, { algorithm: 'sha1', codeDigits: 8, time: 1111111111 })).toEqual('14050471')
-      expect(generate(secret32, { algorithm: 'sha256', codeDigits: 8, time: 1111111111 })).toEqual('67062674')
-      expect(generate(secret64, { algorithm: 'sha512', codeDigits: 8, time: 1111111111 })).toEqual('99943326')
+    expect(generate(secret, { algorithm: 'sha1', codeDigits: 8, time: 1111111111 })).toEqual('14050471')
+    expect(generate(secret32, { algorithm: 'sha256', codeDigits: 8, time: 1111111111 })).toEqual('67062674')
+    expect(generate(secret64, { algorithm: 'sha512', codeDigits: 8, time: 1111111111 })).toEqual('99943326')
 
-      expect(generate(secret, { algorithm: 'sha1', codeDigits: 8, time: 1234567890 })).toEqual('89005924')
-      expect(generate(secret32, { algorithm: 'sha256', codeDigits: 8, time: 1234567890 })).toEqual('91819424')
-      expect(generate(secret64, { algorithm: 'sha512', codeDigits: 8, time: 1234567890 })).toEqual('93441116')
+    expect(generate(secret, { algorithm: 'sha1', codeDigits: 8, time: 1234567890 })).toEqual('89005924')
+    expect(generate(secret32, { algorithm: 'sha256', codeDigits: 8, time: 1234567890 })).toEqual('91819424')
+    expect(generate(secret64, { algorithm: 'sha512', codeDigits: 8, time: 1234567890 })).toEqual('93441116')
 
-      expect(generate(secret, { algorithm: 'sha1', codeDigits: 8, time: 2000000000 })).toEqual('69279037')
-      expect(generate(secret32, { algorithm: 'sha256', codeDigits: 8, time: 2000000000 })).toEqual('90698825')
-      expect(generate(secret64, { algorithm: 'sha512', codeDigits: 8, time: 2000000000 })).toEqual('38618901')
+    expect(generate(secret, { algorithm: 'sha1', codeDigits: 8, time: 2000000000 })).toEqual('69279037')
+    expect(generate(secret32, { algorithm: 'sha256', codeDigits: 8, time: 2000000000 })).toEqual('90698825')
+    expect(generate(secret64, { algorithm: 'sha512', codeDigits: 8, time: 2000000000 })).toEqual('38618901')
 
-      expect(generate(secret, { algorithm: 'sha1', codeDigits: 8, time: 20000000000 })).toEqual('65353130')
-      expect(generate(secret32, { algorithm: 'sha256', codeDigits: 8, time: 20000000000 })).toEqual('77737706')
-      expect(generate(secret64, { algorithm: 'sha512', codeDigits: 8, time: 20000000000 })).toEqual('47863826')
-    })
+    expect(generate(secret, { algorithm: 'sha1', codeDigits: 8, time: 20000000000 })).toEqual('65353130')
+    expect(generate(secret32, { algorithm: 'sha256', codeDigits: 8, time: 20000000000 })).toEqual('77737706')
+    expect(generate(secret64, { algorithm: 'sha512', codeDigits: 8, time: 20000000000 })).toEqual('47863826')
   })
 })
--- tests/verify.test.ts
+++ tests/verify.test.ts
@@ -1,17 +1,15 @@
 import { verify } from '../src'
 
-describe('time-based-one-time-password', (): void => {
-  describe('verify', (): void => {
-    it('verifies a password around the time window given', async (): Promise<void> => {
-      const secret = Buffer.from('3132333435363738393031323334353637383930', 'hex').toString('utf-8')
-      const time = 1111111109
-      const password = '07081804'
+describe(verify, (): void => {
+  it('verifies a password around the time window given', async (): Promise<void> => {
+    const secret = Buffer.from('3132333435363738393031323334353637383930', 'hex').toString('utf-8')
+    const time = 1111111109
+    const password = '07081804'
 
-      expect(verify(password, secret, { codeDigits: 8, time })).toBeTruthy()
-      expect(verify(password, secret, { codeDigits: 8, time: time - 30 })).toBeTruthy()
-      expect(verify(password, secret, { codeDigits: 8, time: time + 30 })).toBeTruthy()
-      expect(verify(password, secret, { codeDigits: 8, time: time + 60 })).toBeFalsy()
-      expect(verify(password, secret, { codeDigits: 8, time: time + 60, offsetSteps: 2 })).toBeTruthy()
-    })
+    expect(verify(password, secret, { codeDigits: 8, time })).toBeTruthy()
+    expect(verify(password, secret, { codeDigits: 8, time: time - 30 })).toBeTruthy()
+    expect(verify(password, secret, { codeDigits: 8, time: time + 30 })).toBeTruthy()
+    expect(verify(password, secret, { codeDigits: 8, time: time + 60 })).toBeFalsy()
+    expect(verify(password, secret, { codeDigits: 8, time: time + 60, offsetSteps: 2 })).toBeTruthy()
   })
 })
